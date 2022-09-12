434c68bc3873766fe28a05ac3601815ed0228c8c
anselminator <103251623+anselminator@users.noreply.github.com>
2022-09-12 15:40:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,46 @@ app.get("/:country/:player", (req, res) => {
     res.status.send(player);
 });
 
+app.delete("/:country/:player", (req, res) => {
+    const countryName = req.params.country;
+    const playerName = req.params.player;
+    const team = players.filter((country) => {
+        return country.country === countryName;
+    })[0];
+    if (!team) {
+        res.status(404).send("error: team " + countryName + " doesnt exist.");
+    }
+    console.log("team =", team);
+    const indexOfTeam = players.findIndex((team) => team.country === countryName);
+
+    const playersNames = team.players.map((p) => p.name);
+    const player = team.players.find((p) => p.name === playerName);
+    if (!player) {
+        res
+            .status(404)
+            .send(
+                "error: player " +
+                playerName +
+                " in team " +
+                countryName +
+                " doesnt exist."
+            );
+    }
+    players[indexOfTeam].players = players[indexOfTeam].players.filter(
+        (p) => p.name != playerName
+    );
+
+    fs.writeFile("./basketballPlayers.json", JSON.stringify(players), (err) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+    });
+
+    console.log("player details ", player);
+    res.status(200).send(player);
+});
+
 app.post("/:country", (req, res) => {
     const { country } = req.params;
     console.log(country);
